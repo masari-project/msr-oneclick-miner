3b755d2a80f11a1585371888b569c422057d0cea
cryptochangements34 <bW9uZXJv@gmail.com>
2018-08-06 23:08:52 -0400

    use external setup.js

---
+++ setup.js
@@ -0,0 +1,27 @@
+"use strict"
+
+var fs = require('fs');
+
+exports.writeConfigTxt = function() {
+    var configText = "\"call_timeout\" : 10, \"retry_time\" : 30, \"giveup_limit\" : 0, \"verbose_level\" : 3, \"print_motd\" : true, \"h_print_time\" : 60, \"aes_override\" : null, \"use_slow_memory\" : \"warn\", \"tls_secure_algo\" : true, \"daemon_mode\" : false, \"flush_stdout\" : false, \"output_file\" : \"\", \"httpd_port\" : 0, \"http_login\" : \"\", \"http_pass\" : \"\", \"prefer_ipv4\" : true,"
+    fs.writeFile("config.txt", configText);
+}
+
+exports.writePoolsTxt = function(address) {
+    var configText = "\"pool_list\" :[ {\"pool_address\" : \"" + pool + "\", \"wallet_address\" : \"" + address + "\", \"rig_id\" : \"oneclick\", \"pool_password\" : \"x\", \"use_nicehash\" : false, \"use_tls\" : false, \"tls_fingerprint\" : \"\", \"pool_weight\" : 1 }, ], \"currency\" : \"masari\","
+    fs.writeFile("pools.txt", configText)
+}
+
+exports.writeCpuTxt = function() {
+    var configText = "\"cpu_threads_conf\" : [ { \"low_power_mode\" : false, \"no_prefetch\" : true, \"affine_to_cpu\" : 0 }, ],"
+    fs.writeFile("cpu.txt", configText);
+}
+
+exports.doConfigsExist = function() {
+    if(!fs.existsSync('config.txt') || !fs.existsSync('pools.txt') || !fs.existsSync('cpu.txt')) {
+      return false;
+    }
+    else {
+      return true;
+    }
+}
